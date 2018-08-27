513d609456fd9ab9daea32cf60bff55d77c1a056
Jack Cunninghm <cunningjc10@gmail.com>
2018-08-27 15:44:14 -0400

    Added sign up form

--- app.js
+++ app.js
@@ -51,13 +51,13 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-
+/*
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("SPD Site Has Started");
 });
+*/
 
-/*
 app.listen(3001, 'localhost', function() {
     console.log("SPD Site Has Started");
   });
-  */
+  
